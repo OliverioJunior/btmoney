5c77dcc84844ff49ec7973c1b09feeba015adef2
Oliverio <oliverio.junior2@gmail.com>
2022-10-23 19:10:18 -0300

    Criar dashboard

--- src/style/global.ts
+++ src/style/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyled = createGlobalStyle`
  :root {
   --red: #E52E40;
+  --green: #33cc95;
   --blue: #5429CC;
 
   --blue-light:#6933FF;
