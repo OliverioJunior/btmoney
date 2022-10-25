48a4be9939c0a7b8a4b3b9e471b0f90d1940499a
Oliverio <oliverio.junior2@gmail.com>
2022-10-25 06:01:28 -0300

    Format data

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -34,9 +34,16 @@ export const TransactionTable: React.FC = () => {
             ({ id, title, type, category, amount, createAt }) => (
               <tr key={id}>
                 <td>{title}</td>
-                <td className={type}>{amount}</td>
+                <td className={type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(amount)}
+                </td>
                 <td>{category}</td>
-                <td>{createAt}</td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR').format(new Date(createAt))}
+                </td>
               </tr>
             ),
           )}
